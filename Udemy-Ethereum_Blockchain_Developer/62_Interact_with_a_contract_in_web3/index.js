89992218c091c1c39045e2448609ca588938be58
KyoungRan K <kyoungrankang@gmail.com>
2018-02-14 10:41:16 +0900

    62 index

--- Udemy-Ethereum_Blockchain_Developer/62_Interact_with_a_contract_in_web3/index.js
+++ Udemy-Ethereum_Blockchain_Developer/62_Interact_with_a_contract_in_web3/index.js
@@ -4,6 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
   <title>MyDapp</title>
+  <script src="web3.js/dist/web3.min.js"></script>
   <script type="text/javascript">
     if (typeof web3 !== 'undefined') {
       web3 = new Web3(web3.currentProvider);
