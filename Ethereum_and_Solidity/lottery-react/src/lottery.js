88217b6ce6dde692e345645f2e90f43c3db2a5a8
KyoungRan K <kyoungrankang@gmail.com>
2018-04-16 14:10:29 +0900

    building Interactive Frontends

---
+++ Ethereum_and_Solidity/lottery-react/src/lottery.js
@@ -0,0 +1,74 @@
+import web3 from './web3';
+
+const address = '0x4d96d35b6A603a16E3BA876ab66B166013509861';
+
+const abi = [
+  {
+    "constant":true,
+    "inputs":[],
+    "name":"manager",
+    "outputs":[{"name":"","type":"address"}],
+    "payable":false,
+    "stateMutability":"view",
+    "type":"function"
+  },
+  {
+    "constant":false,
+    "inputs":[],
+    "name":"pickWinner",
+    "outputs":[],
+    "payable":false,
+    "stateMutability":"nonpayable",
+    "type":"function"
+  },
+  {
+    "constant":true,
+    "inputs":[],
+    "name":"getPlayers",
+    "outputs":[
+      {
+        "name":"",
+        "type":"address[]"
+      }
+    ],
+    "payable":false,
+    "stateMutability":"view",
+    "type":"function"
+  },
+  {
+    "constant":false,
+    "inputs":[],
+    "name":"enter",
+    "outputs":[],
+    "payable":true,
+    "stateMutability":"payable",
+    "type":"function"
+  },
+  {
+    "constant":true,
+    "inputs":[
+      {
+        "name":"",
+        "type":"uint256"
+      }
+    ],
+    "name":"players",
+    "outputs":[
+      {
+        "name":"",
+        "type":"address"
+      }
+    ],
+    "payable":false,
+    "stateMutability":"view",
+    "type":"function"
+  },
+  {
+    "inputs":[],
+    "payable":false,
+    "stateMutability":"nonpayable",
+    "type":"constructor"
+  }
+];
+
+export default new web3.eth.Contract(abi, address);
